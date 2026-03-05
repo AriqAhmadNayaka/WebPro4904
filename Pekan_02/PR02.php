20944dfef61a441f2a9cc311477ff5bbdf54c1f6
davin aileenskie sofiandy <davin.aileenskie1@gmail.com>
2026-03-05 19:38:03 +0700

    Mengumpulkan tugas pekan 02

---
+++ Pekan_02/PR02.php
@@ -0,0 +1,13 @@
+<?php
+
+// Input bilangan desimal
+$desimal = 225;
+
+// Proses konversi desimal ke hexadecimal
+$hexadecimal = dechex($desimal);
+
+// Menampilkan hasil
+echo "Bilangan Desimal : " . $desimal . "\n";
+echo "Bilangan Hexadecimal : " . strtoupper($hexadecimal);
+
+?>
