bb64da037e3e09c77fe7659775065e09cb03b34b
Dryhus21 <dzacky98ku@gmail.com>
2026-03-05 12:49:59 +0700

    Mengumpulkan tugas pekan 02

---
+++ Pekan-02/PR02_02.php
@@ -0,0 +1,13 @@
+<?php
+
+// input angka desimal
+$desimal = 255;
+
+// konversi ke hexadecimal
+$hexa = dechex($desimal);
+
+// tampilkan hasil
+echo "Bilangan Desimal : " . $desimal . "<br>";
+echo "Bilangan Hexadecimal : " . $hexa;
+
+?>
