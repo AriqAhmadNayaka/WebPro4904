2f2197841d9d47a02c0dcff4d7e7563febb92782
fadhilah azzahra <fadilazzah41@gmail.com>
2026-03-05 23:29:05 +0700

    mengumpulkan tugas pekan 01

--- Pekan-02/TP2.php
+++ Pekan-02/TP2.php
@@ -1,13 +1,13 @@
 <?php
 
-function Konvert($desimal){
-    $hex = dechex($desimal);
-    return $hex;
+function Konvert($desimal){ 
+    $hex = dechex($desimal); //untuk membuat fungsi konversi desimal ke heksadesimal
+    return $hex; //membalikan nilai hasil konversi
 }
 
-$desimal = 255;
-$hexa = Konvert($desimal);
+$desimal = 255; //inisiasi nilai desimal yang mmau dikonversi
+$hexa = Konvert($desimal); //manggil fungsi konvert dengan parameter desimal
 
-echo "hasil dari konversi desimal adalah: " . strtoupper($hexa);
+echo "hasil dari konversi desimal adalah: " . strtoupper($hexa); //menampilkan hasil konversi
 
 ?>
