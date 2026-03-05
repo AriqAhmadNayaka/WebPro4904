7f86259e72dd15a9e41c3ffb7dbc8045a2df990a
Levii90 <imhafidhidayat@gmail.com>
2026-03-05 11:11:15 +0700

    mengupload folder pekan 02

---
+++ Pekan-02/Rabu 3 Februari/hitung luas.php
@@ -0,0 +1,8 @@
+<?php
+$rusuk = 10;
+$luas_alas = $rusuk * $rusuk;
+$volume = $rusuk * $rusuk * $rusuk;
+echo "Panjang Rusuk: $rusuk <br>";
+echo "Luas Alas Kubus: $luas_alas <br>";
+echo "Volume Kubus: $volume";
+?>
