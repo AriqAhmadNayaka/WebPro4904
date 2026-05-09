061ff5e26ec98c024dfcb8b87ac2afdce55b637e
zalfaaghaitsaa <zalfaaghaitsaa310@gmail.com>
2026-05-09 23:19:37 +0700

    Mengumpulkan tugas pekan-10

--- Coba/application/modules/Posts/views/footer.php
+++
@@ -1,9 +0,0 @@
-    </div> <!-- End container -->
-    
-    <footer>
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> Posts Management System - Built with CodeIgniter 3</p>
-        </div>
-    </footer>
-</body>
-</html>
