6f3bd00a8bf37e057c22116e20b352ef1c3c61d8
zalfaaghaitsaa <zalfaaghaitsaa310@gmail.com>
2026-05-09 23:06:25 +0700

    Mengumpulkan tugas pekan-10

---
+++ Pekan-10/webstore/src/app.js
@@ -0,0 +1,31 @@
+import { fetchProducts } from "./api/productApi.js";
+import { getState, setState } from "./core/Store.js";
+import { ProductList } from "./components/ProductList.js";
+import { Cart } from "./components/Cart.js";
+import { Newsletter } from "./components/Newsletter.js";
+function addToCart(product) {
+  const state = getState();
+  setState({ cart: [...state.cart, product] });
+  render();
+}
+function checkout() {
+  setState({ cart: [] });
+  alert("Thank you for your purchase!");
+  render();
+}
+function render() {
+  const state = getState();
+  ProductList(state.products, addToCart);
+  Cart(state.cart, checkout);
+}
+async function init() {
+  try {
+    const data = await fetchProducts();
+    setState({ products: data });
+    render();
+    Newsletter();
+  } catch (error) {
+    console.error("Failed to load products:", error);
+  }
+}
+init();
